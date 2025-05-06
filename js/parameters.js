bcd764a1c2dbff653b097a6996ee41940130bfcc
samjwebster <samjwebster01@gmail.com>
2025-05-06 18:39:37 -0400

    final additions

--- js/parameters.js
+++ js/parameters.js
@@ -27,6 +27,7 @@ window.addEventListener('load', function() {
 
                 for (let i = 0; i < alignment_data.length; i++) {
                     let model_name = Object.keys(data)[i];
+                    let model_nickname = data[model_name].nickname;
                     let alignment = alignment_data[i];
                     let stats = stats_data[i];
 
@@ -38,10 +39,13 @@ window.addEventListener('load', function() {
                         count++;
                     }
                     let average_alignment = total_alignment / count;
+                    let stddev_alignment = Math.sqrt(alignment_data[i].reduce((sum, value) => sum + Math.pow(value - average_alignment, 2), 0) / count);
 
                     config_level_data.push({
                         model: model_name,
+                        nickname: model_nickname,
                         alignment: average_alignment,
+                        stddev_alignment: stddev_alignment,
                         stats: stats
                     });
                 }
@@ -49,12 +53,14 @@ window.addEventListener('load', function() {
                 // Sample level: one object per sample, where alignment ratio varies but stats are the same
                 for (let i = 0; i < alignment_data.length; i++) {
                     let model_name = Object.keys(data)[i];
+                    let model_nickname = data[model_name].nickname;
                     let alignment = alignment_data[i];
                     let stats = stats_data[i];
 
                     for (let sample in alignment) {
                         sample_level_data.push({
                             model: model_name,
+                            nickname: model_nickname,
                             sample: sample,
                             alignment: alignment[sample],
                             stats: stats
@@ -113,10 +119,10 @@ function createChart(parameter, datatype) {
 
     let svg = d3.select(chart)
         .append('svg')
-        .attr('width', 450)
-        .attr('height', 450);
+        .attr('width', 480)
+        .attr('height', 480);
 
-    let margin = { top: 40, right: 0, bottom: 20, left: 50 };
+    let margin = { top: 30, right: 0, bottom: 20, left: 50 };
     let width = +svg.attr('width') - margin.left - margin.right;
     let height = +svg.attr('height') - margin.top - margin.bottom;
 
@@ -140,6 +146,30 @@ function createChart(parameter, datatype) {
 
     let xAxis = d3.axisBottom(x).ticks(10);
     let yAxis = d3.axisLeft(y).ticks(10);
+
+    // Append gridlines
+    let gridlinesX = d3.axisBottom(x)
+        .tickSize(-height + margin.top + margin.bottom)
+        .tickFormat('');
+    let gridlinesY = d3.axisLeft(y)
+        .tickSize(-width + margin.left + margin.right)
+        .tickFormat('');
+
+    svg.append('g')
+        .attr('class', 'grid')
+        .attr('stroke', '#ccc')
+        .attr('stroke-width', 0.5)
+        .attr('opacity', 0.25)
+        .attr('transform', `translate(0,${height - margin.bottom})`)
+        .call(gridlinesX);
+
+    svg.append('g')
+        .attr('class', 'grid')
+        .attr('stroke', '#ccc')
+        .attr('stroke-width', 0.5)
+        .attr('opacity', 0.25)
+        .attr('transform', `translate(${margin.left},0)`)
+        .call(gridlinesY);
 
     // Append x-axis
     svg.append('g')
@@ -168,6 +198,7 @@ function createChart(parameter, datatype) {
     } else {
         parameter_label = parameter.charAt(0).toUpperCase() + parameter.slice(1);
     }
+
     svg.append('text')
         .attr('x', width / 2)
         .attr('y', height + margin.bottom - 5)
@@ -181,28 +212,37 @@ function createChart(parameter, datatype) {
         .attr('y', margin.left - 30)
         .attr('text-anchor', 'middle')
         .style('font-size', '14px')
-        .text('Alignment Ratio');
+        .text('Alignment');
 
-    // Title
-    svg.append('text')
-        .attr('x', width / 2)
-        .attr('y', margin.top / 2)
-        .attr('text-anchor', 'middle')
-        .style('font-size', '15px')
-        .style('font-weight', 'bold')
-        .text(`Parameter vs Alignment Ratio: ${parameter_label}`);
+    comparison_title = document.getElementById("comparison-title");
+    comparison_title.innerHTML = `${parameter_label} vs Alignment`;
 
-    // Draw error bars
-    svg.selectAll('.error-bar')
-        .data(filteredData)
-        .enter().append('line')
-        .attr('class', 'error-bar')
-        .attr('x1', d => x(d.stats[parameter][0] - d.stats[parameter][1])) // mean - std
-        .attr('x2', d => x(d.stats[parameter][0] + d.stats[parameter][1])) // mean + std
-        .attr('y1', d => y(d.alignment))
-        .attr('y2', d => y(d.alignment))
-        .attr('stroke', 'gray')
-        .attr('stroke-width', 2);
+    
+    if(datatype == "config") {
+        // Draw horizontal error bars for the parameter
+        svg.selectAll('.error-bar')
+            .data(filteredData)
+            .enter().append('line')
+            .attr('class', 'error-bar')
+            .attr('x1', d => x(d.stats[parameter][0] - d.stats[parameter][1])) // mean - std
+            .attr('x2', d => x(d.stats[parameter][0] + d.stats[parameter][1])) // mean + std
+            .attr('y1', d => y(d.alignment))
+            .attr('y2', d => y(d.alignment))
+            .attr('stroke', 'gray')
+            .attr('stroke-width', 2);
+
+        // Draw vertical error bars for the alignment
+        svg.selectAll('.error-bar-vertical')
+            .data(filteredData)
+            .enter().append('line')
+            .attr('class', 'error-bar')
+            .attr('x1', d => x(d.stats[parameter][0])) // mean
+            .attr('x2', d => x(d.stats[parameter][0])) // mean
+            .attr('y1', d => y(d.alignment - d.stats[parameter][1])) // alignment - std
+            .attr('y2', d => y(d.alignment + d.stats[parameter][1])) // alignment + std
+            .attr('stroke', 'gray')
+            .attr('stroke-width', 2);
+    }
 
 
     // Plot points
@@ -218,13 +258,43 @@ function createChart(parameter, datatype) {
             d3.select(this).style('fill', 'orange');
             let tooltip = d3.select('#tooltip');
             tooltip.style('display', 'block')
-                .html(`Model: ${d.model}<br>Sample: ${d.sample || 'N/A'}<br>${parameter} (mean): ${d.stats[parameter][0]}<br>Alignment: ${d.alignment}`);
+                .html(`Model: ${d.nickname ? d.nickname : d.model}${d.sample ? `<br>Sample: ${d.sample}` : ""}<br>${parameter_label}: ${d.stats[parameter][0].toFixed(3)+"±"+d.stats[parameter][1].toFixed(3)}<br>Alignment: ${datatype=="sample" ? d.alignment.toFixed(3) : d.alignment.toFixed(3)+"±"+d.stddev_alignment.toFixed(3)}`);
         })
         .on('mouseout', function() {
             d3.select(this).style('fill', 'steelblue');
             d3.select('#tooltip').style('display', 'none');
         });
 
+    // Get regression line using ss
+    let x_values = filteredData.map(d => d.stats[parameter][0]);
+    let y_values = filteredData.map(d => d.alignment);
+    let regression_data = x_values.map((x, i) => [x, y_values[i]]);
+
+    let regression = ss.linearRegression(regression_data);
+    let regression_line = ss.linearRegressionLine(regression);  
+    let x1 = d3.min(x_values);
+    let x2 = d3.max(x_values);
+    let y1 = regression_line(x1);
+    let y2 = regression_line(x2);
+
+    svg.append('line')
+        .attr('class', 'regression-line')
+        .attr('x1', x(x1))
+        .attr('y1', y(y1))
+        .attr('x2', x(x2))
+        .attr('y2', y(y2))
+        .attr('stroke', 'red')
+        .attr('stroke-width', 2)
+        .style('stroke-dasharray', '2, 2');
+    
+    let r = ss.sampleCorrelation(x_values, y_values);
+    let strength = Math.abs(r) >= 0.5 ? "strong" : Math.abs(r) >= 0.25 ? "moderate" : "weak";
+    let direction = r > 0 ? "positive" : "negative";
+    let summary = `There is <b>${strength} ${direction}</b> relationship between ${parameter_label} and Alignment.<br>* Correlation Coefficient (r): ${r.toFixed(3)}`;
+    let relationship_text = document.getElementById("relationship");
+    relationship_text.innerHTML = summary;
+   
+        
     // Tooltip creation
     if (d3.select('#tooltip').empty()) {
         d3.select('body').append('div')
@@ -242,6 +312,22 @@ function createChart(parameter, datatype) {
         d3.select('#tooltip')
             .style('left', (event.pageX + 10) + 'px')
             .style('top', (event.pageY - 20) + 'px');
+    });
+
+    // Add cards ordered by alignment in rank-content
+    let rankContent = document.getElementById("rank-content");
+    rankContent.innerHTML = ""; // Clear previous content
+    filteredData.sort((a, b) => b.alignment - a.alignment); // Sort by alignment ratio
+    filteredData.forEach(d => {
+        let rank = filteredData.indexOf(d) + 1;
+        let newCard = `
+        <div class="card" style="background-color: #f9f9f9; width: 95%; border-radius: 10px; padding: 5px; margin: 5px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); display: flex; flex-direction: column; align-items: center;">
+            <h4 style="margin: 0; font-weight: normal;"><b>${rank}:</b> ${d.nickname ? d.nickname : d.model}${d.sample ? `, Sample ${d.sample}` : ""}</h4>
+            <p style="margin: 0;">${parameter_label}: ${d.stats[parameter][0].toFixed(3)}±${d.stats[parameter][1].toFixed(3)}</p>
+            <p style="margin: 0;">Alignment: ${d.alignment.toFixed(3)}${datatype=='config'?"±" + d.stddev_alignment.toFixed(3):""}</p>
+        </div>
+        `;
+        rankContent.innerHTML += newCard;
     });
 }
 
